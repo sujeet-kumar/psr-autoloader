6abe487c6ec38e94d4d1423b40e8e99e6bc2d658
Sujeet Kumar <sujeetkv90@gmail.com>
2016-11-18 15:15:16 +0530

    updated script

--- psr0/Autoloader.php
+++ psr0/Autoloader.php
@@ -1,98 +1,116 @@
 <?php
 /**
  * PSR-0 Autoloader for classes
+ * 
  * @author Sujeet <sujeetkv90@gmail.com>
- * https://github.com/sujeet-kumar/psr-autoloader
+ * @link https://github.com/sujeet-kumar/psr-autoloader
  */
-
 class Autoloader
 {
-	const DS = DIRECTORY_SEPARATOR;
-	
-	protected static $base_include_path = '';
-	protected static $loaded_files = array();
-	
-	protected static $ns_separator = '\\';
-	protected static $file_ext = '.php';
-	protected static $strict_mode = true;
-	
-	/**
-	 * Register the autoloader
-	 * @param	string $base_include_path
-	 * @param	bool $strict_mode
-	 * @param	string $file_ext
-	 * @param	string $ns_separator
-	 */
-	public static function register($base_include_path, $strict_mode = true, $file_ext = '', $ns_separator = ''){
-		self::$base_include_path = rtrim($base_include_path, self::DS);
-		empty($file_ext) or self::$file_ext = $file_ext;
-		empty($ns_separator) or self::$ns_separator = $ns_separator;
-		self::$strict_mode = (bool) $strict_mode;
-		
-		spl_autoload_register(array(get_class(), 'loadClass'));
-	}
-	
-	/**
-	 * Unregister the autoloader
-	 */
-	public static function unregister(){
-		spl_autoload_unregister(array(get_class(), 'loadClass'));
-	}
-	
-	/**
-	 * Get namespace separator
-	 */
-	public static function namespaceSeparator(){
-		return self::$ns_separator;
-	}
-	
-	/**
-	 * Get file extension
-	 */
-	public static function fileExtension(){
-		return self::$file_ext;
-	}
-	
-	/**
-	 * Get loaded files list
-	 */
-	public static function loadedFiles(){
-		return self::$loaded_files;
-	}
-	
-	/**
-	 * Load required class
-	 * @param	string $class_name
-	 */
-	public static function loadClass($class_name){
-		$file_name = '';
-		$namespace = '';
-		
-		if(false !== ($lastNsPos = strripos($class_name, self::$ns_separator))){
-			$namespace = substr($class_name, 0, $lastNsPos);
-			$class_name = substr($class_name, $lastNsPos + 1);
-			$file_name = str_replace('\\', self::DS, $namespace) . self::DS;
-		}
-		
-		$file_name .= str_replace('_', self::DS, $class_name) . self::$file_ext;
-		$full_file_name = self::$base_include_path . self::DS . $file_name;
-		
-		if(file_exists($full_file_name)){
-			self::$loaded_files[] = $full_file_name;
-			require $full_file_name;
-		}elseif(self::$strict_mode){
-			throw new Autoloader_Exception('Class "'.$class_name.'" not found.');
-		}
-	}
-	
-	/* workaround for Autoloader class consistency */
-	protected function __construct(){} /* prevents class instantiation */
-	protected function __clone(){} /* prevents object cloning */
-	protected function __sleep(){} /* prevent object serialization */
-	protected function __wakeup(){} /* prevent object unserialization */
+    const DS = DIRECTORY_SEPARATOR;
+    
+    protected static $base_include_path = '';
+    protected static $loaded_files = array();
+    protected static $ns_separator = '\\';
+    protected static $file_ext = '.php';
+    protected static $strict_mode = true;
+    
+    /**
+     * Register autoloader
+     * @param	string $base_include_path
+     * @param	bool $strict_mode
+     * @param	string $file_ext
+     * @param	string $ns_separator
+     */
+    public static function register($base_include_path, $strict_mode = true, $file_ext = '', $ns_separator = '') {
+        self::$base_include_path = rtrim($base_include_path, self::DS);
+        empty($file_ext) or self::$file_ext = $file_ext;
+        empty($ns_separator) or self::$ns_separator = $ns_separator;
+        self::$strict_mode = (bool) $strict_mode;
+        
+        spl_autoload_register(array(get_class(), 'loadClass'));
+    }
+    
+    /**
+     * Unregister autoloader
+     */
+    public static function unregister() {
+        spl_autoload_unregister(array(get_class(), 'loadClass'));
+    }
+    
+    /**
+     * Get namespace separator
+     */
+    public static function namespaceSeparator() {
+        return self::$ns_separator;
+    }
+    
+    /**
+     * Get file extension
+     */
+    public static function fileExtension() {
+        return self::$file_ext;
+    }
+    
+    /**
+     * Get loaded files list
+     */
+    public static function loadedFiles() {
+        return self::$loaded_files;
+    }
+    
+    /**
+     * Load required class
+     * @param	string $class_name
+     */
+    public static function loadClass($class_name) {
+        $file_name = '';
+        $namespace = '';
+        
+        if (false !== ($lastNsPos = strripos($class_name, self::$ns_separator))) {
+            $namespace = substr($class_name, 0, $lastNsPos);
+            $class_name = substr($class_name, $lastNsPos + 1);
+            $file_name = str_replace('\\', self::DS, $namespace) . self::DS;
+        }
+        
+        $file_name .= str_replace('_', self::DS, $class_name) . self::$file_ext;
+        $full_file_name = self::$base_include_path . self::DS . $file_name;
+        
+        if (file_exists($full_file_name)) {
+            self::$loaded_files[] = $full_file_name;
+            require $full_file_name;
+        } elseif (self::$strict_mode) {
+            throw new AutoloaderException('Class "' . $class_name . '" not found.');
+        }
+    }
+    
+    /* workaround for Autoloader class consistency */
+    
+    /* prevents class instantiation */
+    protected function __construct() {
+        
+    }
+    
+    /* prevents object cloning */
+    protected function __clone() {
+        
+    }
+    
+    /* prevent object serialization */
+    protected function __sleep() {
+        
+    }
+    
+    /* prevent object unserialization */
+    protected function __wakeup() {
+        
+    }
+
 }
 
-/* Autoloader exception class */
-class Autoloader_Exception extends Exception{}
-
-/* End of file Autoloader.php */
+/**
+ * Autoloader exception class
+ */
+class AutoloaderException extends Exception {
+    
+}
--- psr4/Autoloader.php
+++ psr4/Autoloader.php
@@ -4,164 +4,163 @@
  * Also supports explicit class mapping.
  *
  * @author Sujeet <sujeetkv90@gmail.com>
- * https://github.com/sujeet-kumar/psr-autoloader
+ * @link https://github.com/sujeet-kumar/psr-autoloader
  */
-
 class Autoloader
 {
-	const DS = DIRECTORY_SEPARATOR;
-	
-	protected $ns_separator = '\\';
-	protected $file_ext = '.php';
-	
-	protected $ns_prefixes = array();
-	protected $class_map = array();
-	
-	protected $loaded_files = array();
-	
-	protected $strict_mode = true;
-	
-	/**
-	 * Initialize the autoloader
-	 * @param	bool $strict_mode
-	 * @param	string $file_ext
-	 * @param	string $ns_separator
-	 */
-	public function __construct($strict_mode = true, $file_ext = '', $ns_separator = ''){
-		empty($file_ext) or $this->file_ext = $file_ext;
-		empty($ns_separator) or $this->ns_separator = $ns_separator;
-		$this->strict_mode = (bool) $strict_mode;
-	}
-	
-	/**
-	 * Register the autoloader
-	 * @param	bool $prepend
-	 */
-	public function register($prepend = false){
-		spl_autoload_register(array($this, 'loadClass'), true, $prepend);
-	}
-	
-	/**
-	 * Unregister the autoloader
-	 */
-	public function unregister(){
-		spl_autoload_unregister(array($this, 'loadClass'));
-	}
-	
-	/**
-	 * Add base namespace and mapped directory path
-	 * @param	string $ns_prefix
-	 * @param	string $base_dir
-	 * @param	bool $prepend
-	 */
-	public function addNamespace($ns_prefix, $base_dir, $prepend = false){
-		$ns_prefix = trim($ns_prefix, $this->ns_separator) . $this->ns_separator;
-		
-		$base_dir = rtrim($base_dir, self::DS) . self::DS;
-		
-        if(! isset($this->ns_prefixes[$ns_prefix])){
-			$this->ns_prefixes[$ns_prefix] = array();
-		}
-		
-		if($prepend){
-			array_unshift($this->ns_prefixes[$ns_prefix], $base_dir);
-		}else{
-			array_push($this->ns_prefixes[$ns_prefix], $base_dir);
-		}
+    const DS = DIRECTORY_SEPARATOR;
+    
+    protected $ns_separator = '\\';
+    protected $file_ext = '.php';
+    protected $ns_prefixes = array();
+    protected $class_map = array();
+    protected $loaded_files = array();
+    protected $strict_mode = true;
+    
+    /**
+     * Initialize autoloader
+     * @param	bool $strict_mode
+     * @param	string $file_ext
+     * @param	string $ns_separator
+     */
+    public function __construct($strict_mode = true, $file_ext = '', $ns_separator = '') {
+        empty($file_ext) or $this->file_ext = $file_ext;
+        empty($ns_separator) or $this->ns_separator = $ns_separator;
+        $this->strict_mode = (bool) $strict_mode;
     }
-	
-	/**
-	 * Add class map
-	 * @param	array $class_map
-	 */
-	public function addClassMap(array $class_map){
-		$this->class_map = empty($this->class_map) ? $class_map : array_merge($this->class_map, $class_map);
-	}
-	
-	/**
-	 * Load required class
-	 * @param	string $class
-	 */
-	public function loadClass($class){
-		$prefix = $class;
-		$load_success = false;
-		
-		if(isset($this->class_map[$class])){
-			if($this->requireFile($this->class_map[$class])){
-				$this->loaded_files[] = $this->class_map[$class];
-				$load_success = true;
-			}
-		}else{
-			while(false !== $pos = strrpos($prefix, $this->ns_separator)){
-				$prefix = substr($class, 0, $pos + 1);
-				
-				$relative_class = substr($class, $pos + 1);
-				
-				$mapped_file = $this->loadRelativeFile($prefix, $relative_class);
-				
-				if($mapped_file){
-					$this->loaded_files[] = $mapped_file;
-					$load_success = true;
-					break;
-				}
-				
-				$prefix = rtrim($prefix, $this->ns_separator);
-			}
-		}
-		
-		if($load_success === false and $this->strict_mode === true){
-			throw new Autoloader_Exception('Class "'.$class.'" not found.');
-		}
-		return $load_success;
-	}
-	
-	/**
-	 * Get namespace separator
-	 */
-	public function namespaceSeparator(){
-		return $this->ns_separator;
-	}
-	
-	/**
-	 * Get file extension
-	 */
-	public function fileExtension(){
-		return $this->file_ext;
-	}
-	
-	/**
-	 * Get loaded files list
-	 */
-	public function loadedFiles(){
-		return $this->loaded_files;
-	}
-	
-	protected function loadRelativeFile($prefix, $relative_class){
-		if(isset($this->ns_prefixes[$prefix]) === false){
-			return false;
-		}
-		
-		foreach($this->ns_prefixes[$prefix] as $base_dir){
-			$file = $base_dir . str_replace($this->ns_separator, self::DS, $relative_class) . $this->file_ext;
-			
-			if($this->requireFile($file)){
-				return $file;
-			}
-		}
-		
-		return false;
-	}
-	
-	protected function requireFile($file){
-		if(file_exists($file)){
-			require $file;
-			return true;
-		}
-		return false;
-	}
+    
+    /**
+     * Register autoloader
+     * @param	bool $prepend
+     */
+    public function register($prepend = false) {
+        spl_autoload_register(array($this, 'loadClass'), true, $prepend);
+    }
+    
+    /**
+     * Unregister autoloader
+     */
+    public function unregister() {
+        spl_autoload_unregister(array($this, 'loadClass'));
+    }
+    
+    /**
+     * Add base namespace and mapped directory path
+     * @param	string $ns_prefix
+     * @param	string $base_dir
+     * @param	bool $prepend
+     */
+    public function addNamespace($ns_prefix, $base_dir, $prepend = false) {
+        $ns_prefix = trim($ns_prefix, $this->ns_separator) . $this->ns_separator;
+        
+        $base_dir = rtrim($base_dir, self::DS) . self::DS;
+        
+        if (!isset($this->ns_prefixes[$ns_prefix])) {
+            $this->ns_prefixes[$ns_prefix] = array();
+        }
+        
+        if ($prepend) {
+            array_unshift($this->ns_prefixes[$ns_prefix], $base_dir);
+        } else {
+            array_push($this->ns_prefixes[$ns_prefix], $base_dir);
+        }
+    }
+    
+    /**
+     * Add class map
+     * @param	array $class_map
+     */
+    public function addClassMap(array $class_map) {
+        $this->class_map = empty($this->class_map) ? $class_map : array_merge($this->class_map, $class_map);
+    }
+    
+    /**
+     * Load required class
+     * @param	string $class
+     */
+    public function loadClass($class) {
+        $prefix = $class;
+        $load_success = false;
+        
+        if (isset($this->class_map[$class])) {
+            if ($this->requireFile($this->class_map[$class])) {
+                $this->loaded_files[] = $this->class_map[$class];
+                $load_success = true;
+            }
+        } else {
+            while (false !== $pos = strrpos($prefix, $this->ns_separator)) {
+                $prefix = substr($class, 0, $pos + 1);
+                
+                $relative_class = substr($class, $pos + 1);
+                
+                $mapped_file = $this->loadRelativeFile($prefix, $relative_class);
+                
+                if ($mapped_file) {
+                    $this->loaded_files[] = $mapped_file;
+                    $load_success = true;
+                    break;
+                }
+                
+                $prefix = rtrim($prefix, $this->ns_separator);
+            }
+        }
+        
+        if ($load_success === false and $this->strict_mode === true) {
+            throw new AutoloaderException('Class "' . $class . '" not found.');
+        }
+        return $load_success;
+    }
+    
+    /**
+     * Get namespace separator
+     */
+    public function namespaceSeparator() {
+        return $this->ns_separator;
+    }
+    
+    /**
+     * Get file extension
+     */
+    public function fileExtension() {
+        return $this->file_ext;
+    }
+    
+    /**
+     * Get loaded files list
+     */
+    public function loadedFiles() {
+        return $this->loaded_files;
+    }
+    
+    protected function loadRelativeFile($prefix, $relative_class) {
+        if (isset($this->ns_prefixes[$prefix]) === false) {
+            return false;
+        }
+        
+        foreach ($this->ns_prefixes[$prefix] as $base_dir) {
+            $file = $base_dir . str_replace($this->ns_separator, self::DS, $relative_class) . $this->file_ext;
+            
+            if ($this->requireFile($file)) {
+                return $file;
+            }
+        }
+        
+        return false;
+    }
+    
+    protected function requireFile($file) {
+        if (file_exists($file)) {
+            require $file;
+            return true;
+        }
+        return false;
+    }
+
 }
 
-/* Autoloader exception class */
-class Autoloader_Exception extends Exception{}
-
-/* End of file Autoloader.php */
+/**
+ * Autoloader exception class
+ */
+class AutoloaderException extends Exception {
+    
+}
